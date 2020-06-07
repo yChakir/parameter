eb27335778bc4e5ba974abba36f6b33448ffff0b
ychakir <yassine.chakir@ilemgroup.com>
2020-06-07 12:14:44 +0100

    Add project boilerplate. Closes #1

---
+++ src/main/java/ma/tiwtiw/parameter/ParameterApplication.java
@@ -0,0 +1,13 @@
+package ma.tiwtiw.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ParameterApplication.class, args);
+  }
+
+}
